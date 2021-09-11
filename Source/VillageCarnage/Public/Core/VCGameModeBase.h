a11ec1bf4942b67add8d9310bd12f8da22b19bc9
Gyrofighter <gyrofighter@gmail.com>
2021-09-11 19:29:00 +0300

    Initial commit

---
+++ Source/VillageCarnage/Public/Core/VCGameModeBase.h
@@ -0,0 +1,19 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "VCGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class VILLAGECARNAGE_API AVCGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+
+	
+	
+};
